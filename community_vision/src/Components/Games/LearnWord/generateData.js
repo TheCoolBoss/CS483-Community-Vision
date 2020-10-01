485657c252241f84bc3b63072d5693c0bde3fae4
nguyenhu21 <35584482+nguyenhu21@users.noreply.github.com>
2020-09-30 18:00:32 -0700

    generateData.js ver1

--- community_vision/src/Components/Games/LearnWord/generateData.js
+++ community_vision/src/Components/Games/LearnWord/generateData.js
@@ -1,10 +1,56 @@
 /**
  * Looks through all the files in the 'Images' directory
  * Generate a .json file that contains relavent information for LearnWord Game
+ * Each data will have a unique id
  * 
- * NOTE: Correct image needs to have the correct format(All capitalize name, and is a png or jpeg)
- * Example APPLE.png
- * 
+ * Author: Huy Nguyen
  * Created: 9/30/2020
  * Modified: 9/30/2020
  */
+
+
+function generateData () {
+    var fs = require('fs');
+    var path = require('path');
+
+    //Delete all existing .json file
+    fs.unlink('./*.json', (err) => {
+        if (err) throw err;
+    })
+
+    //Create a new .json file name WordGameData.json
+    fs.writeFile('./WordGameData.json', '', (err) => {
+        //Check for errors
+        if (err) throw err;
+    })
+    //Get JSON file
+    const myData = require('./WordGameData.json');
+
+    //Get the images in the directory 'Images'
+    var images = fs.readdirSync('./Images');
+
+    /**
+     * Loop through all the images
+     * push the correct image data to the 
+     */
+    images.forEach((image => {
+        //Check that file is an image
+        if(path.parse(image).ext.localeCompare('.png') === 0 || path.parse(image).ext.localeCompare('.jpg') === 0) {
+            //Variable to store all necessary information from each image
+            let imageData = {
+                name: path.parse(image).name.toUpperCase(),
+                imagePath: './Images/' + image
+            };
+            //Push data to json obj
+            myData.push(imageData);
+        }
+    }));
+
+    //Write the data created to WordGameData.json
+    fs.writeFile('./WordGameData.json', JSON.stringify(myData), err => {
+        //Check for errors
+        if (err) throw err;
+    })
+}
+
+export default generateData;
