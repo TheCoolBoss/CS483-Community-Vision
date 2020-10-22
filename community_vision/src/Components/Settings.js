985e7b7e21213dae5cfc05960d51c97e71150b09
Jordan Ho <hojo21@up.edu>
2020-10-21 23:18:48 -0700

    more visual changes to settings page
    made changes based off client's requests on various settings that should be available for the user

--- community_vision/src/Components/Settings.js
+++ community_vision/src/Components/Settings.js
@@ -29,6 +29,10 @@ function muteSound() {
     volumeMute = !volumeMute;
 }
 
+function valuetext(value) {
+    return `${value}°C`;
+  }
+
 function  Settings() {
     const classes =  useStyles();
     //ToDo: add functionality to volume slider and sound off button click change slider value to 0
@@ -37,6 +41,7 @@ function  Settings() {
         setValue(newValue);
     };
 
+    //ToDo: need to adjust sizing so that the page is relative to rescaling and not pixel specific
     return (
         <div style={{position: 'relative', alignContent: 'center'}} className={classes.root}>
             <h1>Choose Your Game Settings</h1>
@@ -67,10 +72,12 @@ function  Settings() {
                         Choose Colors
                         <ColorLensIcon style={{marginLeft: '5px', marginBottom: '-5px'}}/>
                     </Paper>
-                    <input type="radio" id="colorPackOne" name="colorButton" value="" checked="checked"></input>
-                    <label for="onButton">Color Pack One</label>
-                    <input type="radio" id="colorPackTwo" name="colorButton" value="" style={{marginLeft: '30px'}}></input>
-                    <label for="offButton">Color Pack Two</label>
+                    <input type="radio" id="defaultColorButton" name="colorButton" value="" checked="checked"></input>
+                    <label for="defaultColorButton">Default</label>
+                    <input type="radio" id="greyScaleButton" name="colorButton" value="" style={{marginLeft: '30px'}}></input>
+                    <label for="greyScaleButton">Grey Scale</label>
+                    <input type="radio" id="oneColorButton" name="colorButton" value="" style={{marginLeft: '30px'}}></input>
+                    <label for="oneColorButton">One-Color</label>
                 </Grid>
                 <Grid item xs={6}>
                     <Paper className={classes.paper}>
@@ -84,6 +91,27 @@ function  Settings() {
                         Font Size
                         <FontDownloadIcon style={{marginLeft: '5px', marginBottom: '-5px'}}/>
                     </Paper>
+                    <Grid container spacing={2}>
+                        <Grid  item>
+                            <h2 style={{marginTop: '55px', marginBottom: '-70px'}}>A</h2>
+                        </Grid>
+                        <Grid item xs>
+                            <Slider 
+                                defaultValue={50}
+                                getAriaValueText={valuetext}
+                                aria-labelledby="discrete-slider"
+                                valueLabelDisplay="auto"
+                                step={10}
+                                marks
+                                min={10}
+                                max={100}
+                                style={{marginLeft: '5px', marginBottom: '-70px'}}
+                            />
+                        </Grid>
+                        <Grid  item>
+                            <h2 style={{fontSize: '10vh', marginTop: '5px', marginBottom: '-30px'}}>A</h2>
+                        </Grid>
+                    </Grid>
                 </Grid>
                 <Grid item xs={12}>
                     <Paper className={classes.paper} style={{backgroundColor: '#ffd700'}}>
