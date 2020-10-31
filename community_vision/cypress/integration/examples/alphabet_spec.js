83c0e9b8703f9befce1dc02794a854fc746ca14c
Jordan Ho <hojo21@up.edu>
2020-10-31 14:36:04 -0700

    installed cypress to current branch
    installed cypress testing software and created first test file. need to continue after domain is obtained from client.

---
+++ community_vision/cypress/integration/examples/alphabet_spec.js
@@ -0,0 +1,16 @@
+const { ExpansionPanelActions } = require("@material-ui/core")
+const { cyan } = require("@material-ui/core/colors")
+const { interpolate } = require("react-spring")
+
+describe('Learning Alphabet Test', function () {
+    it('Visits Learning Alphabet Game', function () {
+        //cy.visit('')
+        // Arrange - setup initial app state
+        // - visit Learning Alphabet game page
+        // - query for an element
+        // Act - take an action
+        // - interact with that element
+        // Assert - make an assertion
+        // - make an assertion about page content
+    })
+})
