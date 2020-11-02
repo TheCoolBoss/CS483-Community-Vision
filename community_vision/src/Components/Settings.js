a821bf42cc6d985febf5a6f97bdd144ea3afd08f
Jordan Ho <hojo21@up.edu>
2020-11-02 00:39:52 -0800

    added client requested features to settings page
    added options for background color and font color

--- community_vision/src/Components/Settings.js
+++ community_vision/src/Components/Settings.js
@@ -11,6 +11,7 @@ import AccessAlarmsIcon from '@material-ui/icons/AccessAlarms';
 import FontDownloadIcon from '@material-ui/icons/FontDownload';
 import ColorLensIcon from '@material-ui/icons/ColorLens';
 import HearingIcon from '@material-ui/icons/Hearing';
+import TextField from '@material-ui/core/TextField';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -47,19 +48,20 @@ function  Settings() {
             <h1>Choose Your Game Settings</h1>
             <Grid container spacing={3} style={{position: 'relative', marginRight: '10px', marginLeft: '5px'}}>
                 <Grid item xs={6}>
-                    <Paper className={classes.paper}>
+                    <Paper className={classes.paper} style={{color:"black"}}>
                         Sound
                         <HearingIcon style={{marginLeft: '5px', marginBottom: '-5px'}}/>
                     </Paper>
                     <br></br>
-                    <input type="radio" id="onButton" name="soundButton" value="on" checked="checked"></input>
+                    <input type="radio" id="onButton" name="soundButton" value="" checked="checked"></input>
                     <label for="onButton">Sound On</label>
-                    <input type="radio" id="offButton" name="soundButton" value="off" style={{marginLeft: '30px'}}></input>
+                    <input type="radio" id="offButton" name="soundButton" value="" style={{marginLeft: '30px'}}></input>
                     <label for="offButton">Sound Off</label>
                     <Grid container spacing={2}>
                         <Grid item>
                         <VolumeDown />
                         </Grid>
+                        {/* insert sample sound levels here */}
                         <Grid item xs>
                             <Slider 
                                 defaultValue={50}
@@ -78,8 +80,8 @@ function  Settings() {
                     </Grid>
                 </Grid>
                 <Grid item xs={6}>
-                    <Paper className={classes.paper}>
-                        Choose Colors
+                    <Paper className={classes.paper} style={{color:"black"}}>
+                        Color Packages
                         <ColorLensIcon style={{marginLeft: '5px', marginBottom: '-5px'}}/>
                     </Paper>
                     <br></br>
@@ -90,11 +92,26 @@ function  Settings() {
                     <input type="radio" id="oneColorButton" name="colorButton" value="" style={{marginLeft: '30px'}}></input>
                     <label for="oneColorButton">One-Color</label>
                     <br></br>
-                    <h2>Background Color</h2>
-                    
+                    <br></br>
+                    <Paper className={classes.paper} style={{color:"black"}}>
+                        Background Color
+                        <ColorLensIcon style={{marginLeft: '5px', marginBottom: '-5px'}}/>
+                    </Paper>
+                    <form className={classes.root} noValidate autoComplete="off">
+                        <TextField id="hexCodeBackground" label="Custom Hex Code" />
+                    </form>
+                    <input type="radio" id="blackBackground" name="backgroundButton" value="" checked="checked" style={{marginLeft: '30px'}}></input>
+                    <label for="oneColorButton">Black</label>
+                    <input type="radio" id="greyBackground" name="backgroundButton" value="" style={{marginLeft: '30px'}}></input>
+                    <label for="oneColorButton">Grey</label>
+                    <input type="radio" id="whiteBackground" name="backgroundButton" value="" style={{marginLeft: '30px'}}></input>
+                    <label for="oneColorButton">White</label>
+                    <input type="radio" id="yellowBackground" name="backgroundButton" value="" style={{marginLeft: '30px'}}></input>
+                    <label for="oneColorButton">Yellow</label>
+                    <br></br>
                 </Grid>
                 <Grid item xs={6}>
-                    <Paper className={classes.paper}>
+                    <Paper className={classes.paper} style={{color:"black"}}>
                         Speed
                         <AccessAlarmsIcon style={{marginLeft: '5px', marginBottom: '-5px'}}/>
                     </Paper>
@@ -111,7 +128,7 @@ function  Settings() {
                     />
                 </Grid>
                 <Grid item xs={6}>
-                    <Paper className={classes.paper}>
+                    <Paper className={classes.paper} style={{color:"black"}}>
                         Font Size
                         <FontDownloadIcon style={{marginLeft: '5px', marginBottom: '-5px'}}/>
                     </Paper>
@@ -136,6 +153,24 @@ function  Settings() {
                             <h2 style={{fontSize: '10vh', marginTop: '5px', marginBottom: '-30px'}}>A</h2>
                         </Grid>
                     </Grid>
+                    <br></br>
+                    <Paper className={classes.paper} style={{color:"black"}}>
+                        Font Color
+                        <ColorLensIcon style={{marginLeft: '5px', marginBottom: '-5px'}}/>
+                    </Paper>
+                    <form className={classes.root} noValidate autoComplete="off">
+                        <TextField id="hexCodeFont" label="Custom Hex Code"  />
+                    </form>
+                    <input type="radio" id="blackFont" name="fontButton" value="" checked="checked" style={{marginLeft: '30px'}}></input>
+                    <label for="oneColorButton">Black</label>
+                    <input type="radio" id="whiteFont" name="fontButton" value="" style={{marginLeft: '30px'}}></input>
+                    <label for="oneColorButton">White</label>
+                    <input type="radio" id="redFont" name="fontButton" value="" style={{marginLeft: '30px'}}></input>
+                    <label for="oneColorButton">Red</label>
+                    <input type="radio" id="greenFont" name="fontButton" value="" style={{marginLeft: '30px'}}></input>
+                    <label for="oneColorButton">Green</label>
+                    <input type="radio" id="yellowFont" name="fontButton" value="" style={{marginLeft: '30px'}}></input>
+                    <label for="oneColorButton">Yellow</label>
                 </Grid>
                 <Grid item xs={12}>
                     <Paper className={classes.paper} style={{backgroundColor: '#ffd700'}}>
