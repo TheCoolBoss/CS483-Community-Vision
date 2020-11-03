354564e5c49545464e300204d30125b4b6bcfb67
meremarcinko <42845974+meremarcinko@users.noreply.github.com>
2020-11-02 18:44:39 -0800

    setting button
    added an image for the setting button

--- community_vision/src/Components/Nav.js
+++ community_vision/src/Components/Nav.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../App.css';
 import {Link} from 'react-router-dom';
 import logo from './logo.png'
-import pic from './logopic.png'
+import pic from './settings.png'
 import {useSpring, animated} from 'react-spring'
 
 function Nav() {
