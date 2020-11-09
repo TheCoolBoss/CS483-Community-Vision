ccf88195c41e3787642e042569f39cbe0d22dff4
meremarcinko <42845974+meremarcinko@users.noreply.github.com>
2020-11-08 16:39:13 -0800

    UI
    Added some icons for the games

---
+++ community_vision/src/Components/Games/learnNumbers.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import '../../App.css';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import { Container } from '@material-ui/core';
+import { useSpring, animated } from 'react-spring';
+import {charToMorse, morseToChar} from "./charMorseConv";
+import useSound from 'use-sound';
+import dashSound from '../Assets/Sounds/dash.mp3'
+import dotSound from '../Assets/Sounds/dot.mp3'
