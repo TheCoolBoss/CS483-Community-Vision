22fed5c094e35d38207da5f18c3a0398b0b8fd95
Stephen Nguyen <nguyenst22@up.edu>
2020-11-22 13:21:05 -0800

    Initial now a common function to import

---
+++ community_vision/src/Components/Games/Common/Functions.js
@@ -0,0 +1,17 @@
+//Gets game values fom local storage
+export function initial(type){
+    if(localStorage.getItem(type) != null){
+        return localStorage.getItem(type);
+    }
+    if(type === 'volume'){
+        return 50;
+    } else if(type === 'size'){
+        return 29;
+    } else if(type === 'speed'){
+        return 1.5;
+    } else if(type === 'backgroundColor'){
+        return 'blue';
+    } else if(type === 'fontColor'){
+        return 'white';
+    }
+}
