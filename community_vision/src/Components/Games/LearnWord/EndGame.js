c35bb8bf55fe6cf9eb01274230345d197e134f20
nguyenhu21 <35584482+nguyenhu21@users.noreply.github.com>
2021-01-06 00:29:12 -0800

    Update EndGame.js

--- community_vision/src/Components/Games/LearnWord/EndGame.js
+++ community_vision/src/Components/Games/LearnWord/EndGame.js
@@ -2,9 +2,6 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 
 function EndGame(props) {
-    //history instance to navigate pages
-    // let history = useHistory();
-
     const buttonStyle = {
         backgroundColor: 'white',
         height: '10%',
