762bb321a16b996a96c60c72e4b47117e8b1b368
nguyenhu21 <35584482+nguyenhu21@users.noreply.github.com>
2021-01-14 12:47:26 -0800

    update endgame display

--- community_vision/src/Components/Games/LearnWord/EndGame.js
+++ community_vision/src/Components/Games/LearnWord/EndGame.js
@@ -2,31 +2,36 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 
 function EndGame(props) {
-    const buttonStyle = {
-        backgroundColor: 'white',
-        height: '10%',
-        width: '100%',
-        fontSize: '3vh',
-        marginBottom: 10,
-        padding: 10,
-        display: 'block'
-    };
-
-    const btnContainerStyle = {
-        backgroundColor: 'red', 
-        padding: 75, 
-        display: 'inline-block',
-        position: 'relative',
-        top: '20vh'
-    };
+    const msg = 'Congratulations, you completed ' + props.level + ' level!!'
 
     const overlayStyle = {
         backgroundColor: 'rgba(120, 120, 120, 0.7)',
         height: '90vh',
         width: '100vw',
         position: 'fixed', 
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
         zIndex: 2
     }
+
+    const btnContainerStyle = {
+        backgroundColor: 'red', 
+        padding: 50, 
+        display: 'inline-block',
+        width: '50%',
+        margin: 0
+    };
+
+    const buttonStyle = {
+        backgroundColor: 'white',
+        height: '10%',
+        width: '100%',
+        fontSize: '7vh',
+        marginBottom: 10,
+        padding: 10,
+        display: 'block'
+    };
 
     //Set levels
     var nextLevelPath;
@@ -43,10 +48,13 @@ function EndGame(props) {
     return (
         <div style={overlayStyle}>
             <div style={btnContainerStyle}>
+                <h1>{msg}</h1>
                 <Link to={nextLevelPath}>
-                    <button style={buttonStyle}>Next Level</button>
+                    <button style={buttonStyle}>Play Next Level</button>
                 </Link>
-                <button style={buttonStyle} onClick={() => {window.location.reload()}}>Replay</button>
+                <Link>
+                    <button style={buttonStyle} onClick={() => {window.location.reload()}}>Replay This Level</button>
+                </Link>
                 <Link to='/'>
                     <button style={buttonStyle}>Home Page</button>
                 </Link>
