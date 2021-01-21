358f765e8d43d7ed94ec2d39bba88d6120a9ab4e
nguyenhu21 <35584482+nguyenhu21@users.noreply.github.com>
2021-01-20 17:30:46 -0800

    complete nagivation tests for all games

--- community_vision/cypress/integration/games_navigation_spec.js
+++ community_vision/cypress/integration/games_navigation_spec.js
@@ -21,9 +21,75 @@ describe('Learning Alphabet Game Test', function () {
         // Assert - make an assertion
         // - make an assertion about page content
     })
+    it('Navigates to Learn Alph No Help Game and tests functionality', function () {
+        // Arrange - setup initial app state
+        // - visit Learning Alphabet game page
+        // - query for an element
+        cy.visit('http://localhost:3000/')
+        // Act - take an action
+        // - interact with that element
+        cy.contains('Play Morse!')
+        cy.contains('Play Games!').click()
+        //Check that we are in the settings page
+        cy.url().
+            should('include', '/settings');
+        cy.contains('Play Games!').click()
+        cy.url()
+            .should('include','/games')
+        cy.contains('Alphabet No Help').click()
+        cy.url()
+            .should('include','/noHelpAlphabet')
+        
+        // Assert - make an assertion
+        // - make an assertion about page content
+    })
 })
 
-describe('Word Game Beginner Test', function() {
+describe('Sandbox Games Navigation Test', function() {
+    it('Navigate from home page to sandbox words game', function() {
+        cy.visit('http://localhost:3000');
+        cy.contains('Play Games!').click();
+        cy.url()
+            .should('include', '/settings');
+        cy.contains('Play Games!').click();
+        cy.url()
+            .should('include', '/games');
+        cy.contains('Sandbox Words').click();
+        cy.url()
+            .should('include', '/sandboxWords');
+    })
+
+    it('Navigate from home page to sandbox letters game', function() {
+        cy.visit('http://localhost:3000');
+        cy.contains('Play Games!').click();
+        cy.url()
+            .should('include', '/settings');
+        cy.contains('Play Games!').click();
+        cy.url()
+            .should('include', '/games');
+        cy.contains('Sandbox Letters').click();
+        cy.url()
+            .should('include', '/sandboxLetters');
+        
+    })
+})
+
+describe('Numbers Game Navigation Test', function() {
+    it('Navigate from home page to numbers game', function() {
+        cy.visit('http://localhost:3000');
+        cy.contains('Play Games!').click();
+        cy.url()
+            .should('include', '/settings');
+        cy.contains('Play Games!').click();
+        cy.url()
+            .should('include', '/games');
+        cy.contains('Learn Numbers').click();
+        cy.url()
+            .should('include', '/learnNumbers');
+    })
+})
+
+describe('Word Games Navigation Test', function() {
     it('Navigate from home page to word game beginner page', function() {
         //Visit home page
         cy.visit('http://localhost:3000');
@@ -32,24 +98,18 @@ describe('Word Game Beginner Test', function() {
         //Check that we are in the settings page
         cy.url()
             .should('include', '/settings');
-        //Make sure it contains all setting elements
-        cy.contains('CHOOSE GAME VOLUME');
-        cy.contains('CHOOSE GAME SPEED');
-        cy.contains('CHOOSE TEXT SIZE');
-        cy.contains('PICK A THEME!');
-        cy.contains('CHOOSE BACKGROUND COLOR');
-        cy.contains('CHOOSE COLOR FOR WORDS');
         //Look for element that contains 'Play Games!' and click on it
         cy.contains('Play Games!').click();
+        //Assert that we are in the '/games' page
+        cy.url()
+            .should('include', '/games')
         //Look for element that contains 'Learn Word Easy' and click on it
         cy.contains('Learn Word Easy').click();
         //Check that we are on the correct URL
         cy.url()
             .should('include', '/learnWordBeginner')
-    }) 
-})
+    })
 
-describe('Word Game Medium Test', function() {
     it('Navigate from home page to word game medium page', function() {
         //Visit home page
         cy.visit('http://localhost:3000');
@@ -58,24 +118,18 @@ describe('Word Game Medium Test', function() {
         //Check that we are in the settings page
         cy.url()
             .should('include', '/settings');
-        //Make sure it contains all setting elements
-        cy.contains('CHOOSE GAME VOLUME');
-        cy.contains('CHOOSE GAME SPEED');
-        cy.contains('CHOOSE TEXT SIZE');
-        cy.contains('PICK A THEME!');
-        cy.contains('CHOOSE BACKGROUND COLOR');
-        cy.contains('CHOOSE COLOR FOR WORDS');
         //Look for element that contains 'Play Games!' and click on it
         cy.contains('Play Games!').click();
+        //Assert that we are in the '/games' page
+        cy.url()
+            .should('include', '/games')
         //Look for element that contains 'Learn Word Medium' and click on it
         cy.contains('Learn Word Medium').click();
         //Check that we are on the correct URL
         cy.url()
             .should('include', '/learnWordMedium')
     }) 
-})
 
-describe('Word Game Advanced Test', function() {
     it('Navigate from home page to word game advanced page', function() {
         //Visit home page
         cy.visit('http://localhost:3000');
@@ -84,15 +138,11 @@ describe('Word Game Advanced Test', function() {
         //Check that we are in the settings page
         cy.url()
             .should('include', '/settings');
-        //Make sure it contains all setting elements
-        cy.contains('CHOOSE GAME VOLUME');
-        cy.contains('CHOOSE GAME SPEED');
-        cy.contains('CHOOSE TEXT SIZE');
-        cy.contains('PICK A THEME!');
-        cy.contains('CHOOSE BACKGROUND COLOR');
-        cy.contains('CHOOSE COLOR FOR WORDS');
         //Look for element that contains 'Play Games!' and click on it
         cy.contains('Play Games!').click();
+        //Assert that we are in the '/games' page
+        cy.url()
+            .should('include', '/games')
         //Look for element that contains 'Learn Word Advanded' and click on it
         cy.contains('Learn Word Advanced').click();
         //Check that we are on the correct URL
