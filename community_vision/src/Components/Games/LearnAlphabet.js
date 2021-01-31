c47aa3e7084baa6a52ee7696eab5dbc7540110b4
kamala21 <kamala21@up.edu>
2021-01-30 23:36:01 -0800

    Update LearnAlphabet.js

--- community_vision/src/Components/Games/LearnAlphabet.js
+++ community_vision/src/Components/Games/LearnAlphabet.js
@@ -17,25 +17,6 @@ var t;
 var list = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var textIndex = 0;
 
-function initial(type) {
-    if (localStorage.getItem(type) != null) {
-        return localStorage.getItem(type);
-    }
-    if (type === 'volume') {
-        return 50;
-    } else if (type === 'size') {
-        return 29;
-    } else if (type === 'speed') {
-        return 1.5;
-    } else if (type === 'backgroundColor') {
-        return 'blue';
-    } else if (type === 'fontColor') {
-        return 'white';
-    } else if (type === 'button') {
-        return 'blue';
-    }
-}
-
 function showImage() {
     var x = document.getElementById("tutorialImage");
     if (x.style.display === "none") {
