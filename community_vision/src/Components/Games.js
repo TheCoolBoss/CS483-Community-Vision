b123166a14bfa0463c39f2ce8b0b9feaaec3167c
TheCoolBoss <nguyenst22@up.edu>
2021-02-09 09:56:39 -0800

    Fixed game menu rendering

--- community_vision/src/Components/Games.js
+++ community_vision/src/Components/Games.js
@@ -51,18 +51,13 @@ const Games = forwardRef((props, ref) => {
       <Container maxWidth='xl' style={{ backgroundColor: backgroundColor }}>
         <Grid container justify='center' spacing={2}>
           <Grid item xs={6} sm={4} md={3} lg={2} xl={1}>
-            <GameSelection name='Learn Buttons' img={alphabetIMG} link='/buttons' difficulty='Beginner' />
+            <GameSelection name='Learn Buttons' img={alphabetIMG} link='/buttons' difficulty='Explore' />
           </Grid>
           <Grid item xs={6} sm={4} md={3} lg={2} xl={1}>
-            <GameSelection name='Learn The Alphabet' img={alphabetIMG} link='/learnAlphabet' difficulty='Beginner' />
-          </Grid>
-          <Grid item xs={6} sm={4} md={3} lg={2} xl={1}>
-            <GameSelection name='Learn The Sorted Alphabet' img={alphabetIMG} link='/sorted' difficulty='Beginner' />
-          </Grid>
-          <Grid item xs={6} sm={4} md={3} lg={2} xl={1}>
-            <GameSelection name='Learn Numbers' img={LearnAlphabetIMG} link='/learnNumbers' difficulty='Beginner' />
-
             <GameSelection name='Learn The Alphabet' img={alphabetIMG} link='/learnAlphabet' difficulty='Explore' />
+          </Grid>
+          <Grid item xs={6} sm={4} md={3} lg={2} xl={1}>
+            <GameSelection name='Learn The Sorted Alphabet' img={alphabetIMG} link='/sorted' difficulty='Explore' />
           </Grid>
           <Grid item xs={6} sm={4} md={3} lg={2} xl={1}>
             <GameSelection name='Learn Numbers' img={numbersIMG} link='/learnNumbers' difficulty='Explore' />
