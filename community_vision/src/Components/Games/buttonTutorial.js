c0b21afc722b6b41865213e6048908c7076354c1
TheCoolBoss <nguyenst22@up.edu>
2021-02-10 18:22:18 -0800

    Basic tutorials for my games

--- community_vision/src/Components/Games/buttonTutorial.js
+++ community_vision/src/Components/Games/buttonTutorial.js
@@ -3,10 +3,46 @@ import '../../App.css';
 import useSound from 'use-sound';
 import dashSound from '../Assets/Sounds/dash.mp3'
 import dotSound from '../Assets/Sounds/dot.mp3'
-import {animated} from 'react-spring';
+import {animated, useSpring} from 'react-spring';
 import {initial, Buttons, resetInputTime, resetInputLength} from "./Common/Functions";
+import spacebar from "../Assets/Images/spacebar.png";
+import enterButton from "../Assets/Images/enterButton.png";
+import {Container} from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
 
 var t;
+
+var textIndex = 0;
+
+function updateTutorial() {
+    var text = document.getElementById('tutorialText').innerHTML;
+    var space = document.getElementById('spaceImage');
+    var enter = document.getElementById('enterImage');
+
+    if (textIndex == 0) {
+        document.getElementById('tutorialText').innerHTML = 'This game consists of two buttons at the bottom of the page.';
+
+        textIndex++;
+    } else if (textIndex == 1) {
+        document.getElementById('tutorialText').innerHTML = 'This button is used for the dots and can be accessed through the space button or by clicking here!';
+        document.getElementById('dotButton').style.backgroundColor = "yellow";
+        space.style.display = "block";
+        textIndex++;
+    } else if (textIndex == 2) {
+        document.getElementById('dotButton').style.backgroundColor = document.getElementById('dashButton').style.backgroundColor;
+        document.getElementById('tutorialText').innerHTML = 'This button is used for the dashes and can be accessed through the enter button or by clicking here!';
+        document.getElementById('dashButton').style.backgroundColor = "yellow";
+        space.style.display = "none";
+        enter.style.display = "block";
+        textIndex++;
+    } else if (textIndex == 3) {
+        document.getElementById('dashButton').style.backgroundColor = document.getElementById('dotButton').style.backgroundColor;
+        document.getElementById('tutorialText').innerHTML = 'Click any button and see its output appear on screen!';
+        //document.getElementById('sampleMorse').style.backgroundColor = "yellow";
+        enter.style.display = "none";
+        textIndex = 0;
+    }
+}
 
 const ButtonsTutorial = forwardRef((props, ref) => {
     var [input, setInput] = React.useState('');
@@ -67,6 +103,15 @@ const ButtonsTutorial = forwardRef((props, ref) => {
             gridTemplateAreas: '"top" "middle" "bottom'
         }}>
             <div style={{gridArea: 'top'}}>
+                <div style={{ position: 'absolute' }}>
+                    <Container>
+                        <Grid container justify='left'>
+                            <Grid item>
+                                <Radio />
+                            </Grid>
+                        </Grid>
+                    </Container>
+                </div>
                 <div>
                     <animated.h1 style={{
                         lineHeight: 0,
@@ -87,5 +132,56 @@ const ButtonsTutorial = forwardRef((props, ref) => {
         </div>
     );
 })
+
+const Radio = () => {
+    const [isToggled, setToggle] = useState(false);
+    const menubg = useSpring({ background: isToggled ? "#6ce2ff" : "#ebebeb" });
+    const { y } = useSpring({
+        y: isToggled ? 180 : 0
+    });
+    const menuAppear = useSpring({
+        transform: isToggled ? "translate3D(0,0,0)" : "translate3D(0,-40px,0)",
+        opacity: isToggled ? 1 : 0
+    });
+
+    return (
+        <div style={{ position: "relative", width: "300px", margin: "0 auto" }}>
+            <animated.button
+                style={menubg}
+                className="radiowrapper"
+                onClick={() => setToggle(!isToggled)}
+            >
+                <div className="radio">
+                    <p>Tutorial</p>
+                    <animated.p
+                        style={{
+                            transform: y.interpolate(y => `rotateX(${y}deg)`)
+                        }}
+                    >
+                        ▼
+                    </animated.p>
+                </div>
+            </animated.button>
+            <animated.div style={menuAppear}>
+                {isToggled ? <RadioContent /> : null}
+            </animated.div>
+        </div>
+    );
+};
+
+const RadioContent = () => {
+    return (
+        <div className="radiocontent" >
+            <a href="#" alt="Home">
+            </a>
+            <p id="tutorialText" value="Change Text">Welcome to the Buttons tutorial! </p>
+            <img src={spacebar} alt="Spacebar" id="spaceImage" style={{ display: "none" }}></img>
+            <img src={enterButton} alt="Enter Button" id="enterImage" style={{ display: "none" }}></img>
+            <button onClick={function () {
+                updateTutorial();
+            }} style={{ fontSize: '5vh' }}>Next</button>
+        </div>
+    );
+};
 
 export default ButtonsTutorial;
