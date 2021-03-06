1ad94ca0939882b5e05271bd1f36e602388938b9
meremarcinko <42845974+meremarcinko@users.noreply.github.com>
2021-03-06 15:28:02 -0800

    updated Numbers
    did some stuff

--- community_vision/src/Components/Games/learnNumbers.js
+++ community_vision/src/Components/Games/learnNumbers.js
@@ -2,6 +2,8 @@ import React, {forwardRef, useImperativeHandle, useState} from 'react';
 import '../../App.css';
 import { useSpring, animated } from 'react-spring';
 import {charToMorse, morseToChar} from "./charMorseConv";
+import { Container } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
 import useSound from 'use-sound';
 import dashSound from '../Assets/Sounds/dash.mp3';
 import dotSound from '../Assets/Sounds/dot.mp3';
@@ -23,49 +25,55 @@ var list = "0123456789" //list that you go through
 var textIndex = 0;
 
 
-// function showImage() {
-//     var x = document.getElementById("tutorialImage");
-//     if(x.style.display === "none") {
-//         x.style.display = "block";
-//     } else {
-//         x.style.displau = "none";
-//     }
-// }
+function showImage() {
+    var x = document.getElementById("tutorialImage");
+    if(x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.displau = "none";
+    }
+}
 
-// function updatetutorial() {
-//     var text = document.getElementById('tutorialText').innerHTML;
-//     var space = document.getElementById('spaceImage');
-//     var enter = document.getElementById('enterImage');
+//each textIndex is a "slide" in the tutorial
+function updateTutorial() {
+    var space = document.getElementById('spaceImage');
+    var enter = document.getElementById('enterImage');
 
-//     if (textIndex == 0) {
-//         document.getElementById('tutorialText').innerHTML = 'This game consists of two buttons at the bottom of the page';
-//         textIndex++;
-//     } else if (textIndex == 1) {
-//         document.getElementById('tutorialText').innerHTML = 'This button is used for the dots and can be accessed through the space button or by clicking here!';
-//         document.getElementById('dotButton').style.backgroundColor = "yellow";
-//         document.style.display = "block";
-//         textIndex++;
-//     } else if (textIndex == 2) {
-//         document.getElementById('dotButton').style.backgroundColor = document.getElementById('dashButton').style.backgroundColor;
-//         document.getElementById('tutorialText').innerHTML = 'THis button is used for the dashes and can be accessed through the enter button or by clicking here!';
-//         document.getElementById('dashButton').style.background = "yellow";
-//         space.style.display = "none";
-//         enter.style.display = "block";
-//         textIndex++;
-//     } else if (textIndex == 3) {
-//         document.getElementById('dashButton').style.backgroundColor = document.getElementById('dotButton').style.backgroundColor;
-//         document.getElementById('tutorialText').innerHTML = 'Enter the correct Morse Code shown here!';
-//         document.getElementById('sampleMorse').style.backgroundColor = "yellow";
-//         enter.style.display = "none";
-//         textIndex++;
-//     } else if (textIndex == 4) {
-//         document.getElementById('sampleMorse').style.backgroundColor = document.getElementById('dashButton').style.backgroundColor;
-//         document.getElementById('tutorialText').innerHTML = 'Enter the correct code and move onto the next letter. Have Fun Learning the Morse Alphabet!';
-//         textIndex = 0;
-//     }
-// }
+    if (textIndex == 0) {
+        document.getElementById('tutorialText').innerHTML = 'This game consists of two buttons at the bottom of the page';
+        textIndex++;
+    } else if (textIndex == 1) {
+        document.getElementById('tutorialText').innerHTML = 'This button is used for the dots and can be accessed through the space button or by clicking here!';
+        document.getElementById('dotButton').style.backgroundColor = "yellow";
+        space.style.display = "block";
+        textIndex++;
+    } else if (textIndex == 2) {
+        document.getElementById('dotButton').style.backgroundColor = document.getElementById('dashButton').style.backgroundColor;
+        document.getElementById('tutorialText').innerHTML = 'This button is used for the dashes and can be accessed through the enter button or by clicking here!';
+        document.getElementById('dashButton').style.backgroundColor = "yellow";
+        space.style.display = "none";
+        enter.style.display = "block";
+        textIndex++;
+    } else if (textIndex == 3) {
+        document.getElementById('dashButton').style.backgroundColor = document.getElementById('dotButton').style.backgroundColor;
+        document.getElementById('tutorialText').innerHTML = 'Enter the correct Morse Code shown here!';
+        document.getElementById('sampleMorseCode').style.color = document.getElementById('dotButton').style.backgroundColor;
+        enter.style.display = "none";
+        textIndex++;
+    } else if (textIndex == 4) {
+        // change this in your tutorials to change the color of the divs
+        document.getElementById('sampleMorseCode').style.color = document.getElementById('dotButton').style.color;
+        document.getElementById('tutorialText').innerHTML = 'Enter the correct code and move onto the next number. Have Fun Learning Morse Numbers!';
+        textIndex++;
+        // change color back to regular
+    } else if (textIndex == 5) {
+        // changes smaple morse back to normal color
+        document.getElementById('sampleMorse').style.color = document.getElementById('dashButton').style.backgroundColor;
+        textIndex = 0;
+    }
+}
 
-//
+
 const LearnNumbers = forwardRef((props, ref) => {
     var [randomNumber, setRandomNumber] = useState(0);
     var [index, setIndex] = useState(0); //hooks: everytime you change a hook the page reloads
@@ -124,9 +132,11 @@ const LearnNumbers = forwardRef((props, ref) => {
         if (evt.keyCode === 32) {
             setInput(input + '•');
             playDot();
+            document.getElementById('dotButton').focus();
         } else if (evt.keyCode === 13) {
             setInput(input + '-');
             playDash();
+            document.getElementById('dashButton').focus();
         }
     };
 
@@ -134,6 +144,7 @@ const LearnNumbers = forwardRef((props, ref) => {
     var d = 2000;
     //if not, then it will set the animation to 0 and doesnt do anything, doesnt animate 
     //when its not suppose to be animating
+    //animation for the number displayed
     if (!anim) {
         d = 0;
         t = setTimeout(function () {
@@ -168,17 +179,30 @@ const LearnNumbers = forwardRef((props, ref) => {
             gridTemplateAreas: '"top" "middle" "bottom'
         }}>
             <div style={{gridArea: 'top'}}>
-                <div>
+                <div style={{ position: 'absolute' }}>
+                    <Container>
+                        <Grid container justify='left'>
+                            <Grid item>
+                                <Radio />
+                            </Grid>
+                        </Grid>
+                    </Container>
+                </div>
+                <div id="sampleMorse">
                     <animated.h1 style={{
                         lineHeight: 0,
                         color: fontColor,
                         fontSize: fSize,
+                        pointer: 'default',
+                        userSelect: 'none',
                         opacity: x.interpolate({range: [0, 1], output: [0, 1]})
                     }}>{currentNumber}</animated.h1>
-                    <animated.p style={{
+                    <animated.p id="sampleMorseCode" style={{
                         lineHeight: 0,
                         color: fontColor,
                         fontSize: sfSize,
+                        pointer: 'default',
+                        userSelect: 'none',
                         opacity: x.interpolate({range: [0, 1], output: [0, 1]})
                     }}>{currentMorse}</animated.p>
                 </div>
@@ -197,5 +221,56 @@ const LearnNumbers = forwardRef((props, ref) => {
         </div>
     );
 })
+
+const Radio =() => {
+    const [isToggled, setToggle] = useState(false);
+    const menubg = useSpring({ background: isToggled ? "#6ce2ff" : "#ebebeb" });
+    const { y } = useSpring({
+        y: isToggled ? 180 :0
+    });
+    const menuAppear = useSpring({
+        transform: isToggled ? "translate3D(0,0,0)" : "translate3D(0,-40px,0)",
+        opacity: isToggled ? 1 : 0
+    });
+    return (
+        <div style={{ position: "relative", width: "300px", margin: "0 auto" }}>
+            <animated.button
+                style={menubg}
+                className="radiowrapper"
+                onMouseDown={() => setToggle(!isToggled)}
+            >
+                <div className="radio">
+                    <p>Tutorial</p>
+                    <animated.p
+                        style={{
+                            transform: y.interpolate(y => `rotateX(${y}deg)`)
+                        }}
+                    >
+                        ▼
+                    </animated.p>
+                </div>
+            </animated.button>
+            <animated.div style={menuAppear}>
+                {isToggled ? <RadioContent /> : null}
+            </animated.div>
+        </div>
+    );
+};
+
+// use state object and set it to 0 initially 
+const RadioContent = () => {
+    return (
+        <div className="radiocontent" >
+            <a href="#" alt="Home">
+            </a>
+            <button id='4' onMouseDown={function () {
+                updateTutorial();
+            }} style={{ fontSize: '5vh' }}>Next</button>
+            <p id="tutorialText" value="Change Text">Welcome to the Learn Alphabet Game! This game teaches you the Morse Code Alphabet! </p>
+            <img src={spacebar} alt="Spacebar" id="spaceImage" style={{ display: "none" }}></img>
+            <img src={enterButton} alt="Enter Button" id="enterImage" style={{ display: "none" }}></img>
+        </div>
+    );
+};
 
 export default LearnNumbers;
