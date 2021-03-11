985010530dca54d2c0ba679aaa1e752e1c6fdb57
TheCoolBoss <nguyenst22@up.edu>
2021-03-11 11:27:33 -0800

    More tests

--- community_vision/cypress/integration/charMorseConv_spec.js
+++ community_vision/cypress/integration/charMorseConv_spec.js
@@ -1,6 +1,12 @@
+import {charToMorse, morseToChar} from "../../src/Components/Games/charMorseConv";
+var list = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 describe('Unit tests for Morse to Char conversions', () => {
     context('morseToChar', () => {
-        
+        for (var i = 0; i < list.length; i++)
+        {
+            var morse = charToMorse(list.charAt(i));
+            expect(morseToChar(morse)).equal(list.charAt(i));
+        }
     })
 })
